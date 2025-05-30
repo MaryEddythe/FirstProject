5f8c818f185d5559ad542eaf0fa86ac173d6d728
MaryEddythe <sornito.maryeddythe@gmail.com>
2025-05-30 18:08:12 +0800

    first native app

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [currentQuestions, setCurrentQuestions] = useState(0);
+
+  const quizData = [
+    {
+      question: "What is the most used language in the world?",
+      options: ['Latin', 'French', 'Tagalog', 'Mandarin'],
+      answer: 'Mandarin'
+    },
+    {
+      question: "What is the national tree of the Philippines?",
+      options: ['Narra', 'Pilapil', 'Mahogany', 'Mango'],
+      answer: 'Narra'
+    },
+  ]
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.questionContainer}>
+        <Text>{quizData[currentQuestions]?.question}</Text>
+        {quizData[currentQuestions]?.options.map}
+      </View>
     </View>
   );
 }
@@ -17,4 +35,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  questionContainer: {
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+    width: '80%', 
+  }
 });
+
